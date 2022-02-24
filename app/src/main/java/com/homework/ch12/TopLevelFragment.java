49e9505e6844206b40611ddade2e6f1056174044
Nga Pyaw Tee <ngapyawtee02@gmail.com>
2022-02-25 00:09:30 +0630

    second commit

---
+++ app/src/main/java/com/homework/ch12/TopLevelFragment.java
@@ -0,0 +1,17 @@
+package com.homework.ch12;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopLevelFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_top_level, container, false);
+    }
+}
