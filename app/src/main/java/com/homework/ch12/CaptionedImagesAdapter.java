b0d226d044947cbfb2bd7e35355ffbce00b633b9
Nga Pyaw Tee <ngapyawtee02@gmail.com>
2022-03-01 23:28:20 +0630

    2nd commit for Adapter

--- app/src/main/java/com/homework/ch12/CaptionedImagesAdapter.java
+++ app/src/main/java/com/homework/ch12/CaptionedImagesAdapter.java
@@ -11,36 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
-    private String[] captions;
-    private int[] imageID;
 
-    public CaptionedImagesAdapter(String[] captions, int[] imageID) {
-        this.captions = captions;
-        this.imageID = imageID;
-    }
-
-    @Override
-    public int getItemCount() {
-        return captions.length;
-    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private CardView cardView;
-        public ViewHolder (CardView v){
-            super(v);
-            cardView=v;
-        }
-    }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        CardView cv = (CardView) LayoutInflater.from(viewGroup.getContext()).inflate(
-                R.layout.card_captioned_image,viewGroup,false
-        );
-
-        return new ViewHolder(cv);
     }
 
 
