89a23b923265e6f2353c9af67ed80098e2a2d845
Nga Pyaw Tee <ngapyawtee02@gmail.com>
2022-03-01 23:29:09 +0630

    2nd commit for Adapter

--- app/src/main/java/com/homework/ch12/CaptionedImagesAdapter.java
+++ app/src/main/java/com/homework/ch12/CaptionedImagesAdapter.java
@@ -13,6 +13,22 @@ import android.widget.TextView;
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
 
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
 
